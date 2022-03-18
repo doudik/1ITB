d4d21a5c549a3e6ddfd66734f442d815c8e9e52e
Miroslav Douda <miramirinda@outlook.cz>
2022-03-18 11:34:46 +0100

    mixer

---
+++ 1ITB_S1/PVA/18.3.22/test_projekt_1ITB_mixer/Mixer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace test_projekt_1ITB_mixer
+{
+    internal class Mixer
+    {
+        public float otacky { get; private set; }
+        public float pitch
+        {
+            get { return otacky; }
+            set {
+                Console.WriteLine("Změnil jsem otáčky!");
+                otacky = value; }
+        }
+        public float vykon;
+        public float objem;
+        public int zarucniDoba;
+        public int cena;
+        public bool isOn;
+        List<string> mixerList = new List<string>();
+
+
+        public float getOtacky() {
+            return otacky;
+        }
+        public void setOtacky(float val) {
+            otacky = val;
+        }
+        public Mixer(float otacky, float vykon, float objem, int cena) {
+            this.cena = cena;
+            this.otacky = otacky;
+            this.vykon = vykon;
+            this.objem = objem;
+            zarucniDoba = 24;
+            isOn = false;
+        }
+        void NaplnMixer(string item) {
+            mixerList.Add(item);
+        }
+        public bool ZapniMixer() { 
+            return this.isOn = true;
+        }
+    }
+}
---
+++ 1ITB_S1/PVA/18.3.22/test_projekt_1ITB_mixer/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace test_projekt_1ITB_mixer;
+
+class Program {
+
+    static void Main(string[] args)
+    {
+        Mixer tesla3000 = new Mixer(20.0f, 240f, 1500f, 2400);
+        Mixer ingrid = new Mixer(20.0f, 240f, 1500f, 2400);
+        tesla3000.ZapniMixer();
+        Console.WriteLine("Zapl sem mixér? " + tesla3000.isOn);
+        Console.WriteLine("Zapl sem mixér? " + ingrid.isOn);
+        Console.WriteLine("Nový mixér tesla3000 je právě za cenu {0}", tesla3000.cena);
+        tesla3000.getOtacky();
+    }
+}
