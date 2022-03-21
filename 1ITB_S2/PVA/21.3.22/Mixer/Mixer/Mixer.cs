634e9b0afd76f336f3c27ce3d699a397761ef023
Miroslav Douda <miramirinda@outlook.cz>
2022-03-21 09:44:52 +0100

    updatE

---
+++ 1ITB_S2/PVA/21.3.22/Mixer/Mixer/Mixer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mixer
+{
+    internal class Mixer
+    {
+        public int otacky { 
+            get {
+                return otacky;
+            } 
+            private set { 
+            }
+        }
+        
+        int vaha;
+        string material;
+        public int vykon;
+        public string znacka;
+        public bool isOn;
+
+        //string[] polepotravin = { "okurka", "jablko", "banán", "chleba", "párek" };
+        List<string> potraviny;
+
+        public Mixer(int otacky, int vaha, string material, int vykon, string znacka) { 
+            this.otacky = otacky;
+            this.vaha = vaha;
+            this.material = material;
+            this.vykon = vykon;
+            this.znacka = znacka;
+            isOn = false;
+        }
+        void PridejPotravinu(string arg) {
+            potraviny.Add(arg);
+            Console.WriteLine("Přidal jsem potravinu " + arg);
+        }
+        void OdeberPotravinu(string arg) {
+            potraviny.Remove(arg);
+            Console.WriteLine("Odebral jsem potravinu " + arg );
+        }
+        public void ZapniMixer() {
+            isOn = true;
+            Console.WriteLine("Zapl jsem mixér");
+        }
+        void VypniMixer() {
+            isOn = false;
+            Console.WriteLine("Vypl jsem mixér");
+        }
+       
+
+    }
+}
---
+++ 1ITB_S2/PVA/21.3.22/Mixer/Mixer/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Mixer;
+
+using System.Collections;
+
+class Program {
+    static void Main(string[] args) {
+        Mixer tefal3000 = new Mixer(100, 2000, "plast", 150, "tefal");
+        Mixer parkovac5000 = new Mixer(2000, 4000, "kov", 400, "Salt Bae");
+        
+        //tefal3000.ZapniMixer();
+        Console.WriteLine(tefal3000.otacky);
+        //parkovac5000.ZapniMixer();
+
+        //Console.WriteLine(parkovac5000.isOn);
+    }
+}
