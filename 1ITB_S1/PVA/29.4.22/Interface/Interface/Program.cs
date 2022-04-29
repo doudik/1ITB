2685231272992182485dfbab4a0e32ec63b30259
Miroslav Douda <miramirinda@outlook.cz>
2022-04-29 11:43:52 +0200

    getter, setter, interface, static

---
+++ 1ITB_S1/PVA/29.4.22/Interface/Interface/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Interface {
+    class Program {
+        static void Main(string[] args) {
+            Ucet PepaNovak = new Ucet("Pepa Novak");
+            Ucet u = new Ucet("u");
+            Ucet u2 = new Ucet("v");
+
+
+            //PepaNovak.zustatek = 2200;
+            Console.WriteLine("1. " + PepaNovak.Mena);
+            PepaNovak.Mena = "EUR";
+            Console.WriteLine("2. " + PepaNovak.Mena);
+            Console.WriteLine(PepaNovak.zustatek);
+
+            Console.WriteLine("Počet uživatelů: " + Ucet.pocetUzivatelu);
+            //Console.WriteLine("Kupon: " + PepaNovak.kupon);
+            Ucet.GenerujKupon();
+            Ucet.GenerujKupon();
+            Ucet.UkazKupony();
+        }
+    }
+}
---
+++ 1ITB_S1/PVA/29.4.22/Interface/Interface/Ucet.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Ucet
+    {
+        string mena;
+        public string Mena {
+            get { return mena; }
+            set { ZmenMenu(value);}
+        }
+        public int zustatek { get; private set; }
+        string vlastnik;
+        public static int pocetUzivatelu;
+        //public string kupon { get { return GenerujKupon(); } }
+        static List<string> kuponList = new List<string>();
+
+        public Ucet(string vlastnik) {
+            this.vlastnik = vlastnik;
+            mena = "CZK";
+            zustatek = 200;
+            pocetUzivatelu++;
+        }
+        void ZmenMenu(string value)
+        {
+            if (value == "EUR")
+            {
+                if (mena != "EUR")
+                {
+                    zustatek /= 25;
+                    mena = value;
+                }
+            }
+            else if (value == "CZK")
+            {
+                if (mena != "CZK")
+                {
+                    zustatek *= 25;
+                    mena = value;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Tento typ měny nepodporujeme!");
+            }
+        }
+        public static void GenerujKupon() {
+            Random rnd = new Random();
+            string znaky = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            string tmp = "";
+            for (int i = 1; i < 30; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    tmp += "-";
+                }
+                else{
+                    tmp += znaky[rnd.Next(0, znaky.Length)];
+                }
+            }
+            kuponList.Add(tmp);
+        }
+        public static void UkazKupony() {
+            foreach (var item in kuponList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static bool OverKupon() {
+            Console.WriteLine("Zadejte mi Váš kupon: ");
+            string kupon = Console.ReadLine();
+            foreach (var item in kuponList)
+            {
+                if (kupon == item) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
