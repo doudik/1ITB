5635a53d197c37892c0d3c5c0a117cb3b72052a8
Miroslav Douda <miramirinda@outlook.cz>
2022-05-02 10:00:10 +0200

    písemná práce

---
+++ 1ITB_S2/PVA/02.05.22/PisemnaPrace/PisemnaPrace/Matrika.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PisemnaPrace
+{
+    internal class Matrika
+    {
+        public string jmeno { get; private set; }
+        public string prijmeni { get; private set; }
+        string rodneCislo, datumNarozeni;
+
+        public static List<Matrika> matrikaList = new List<Matrika>();
+
+        public Matrika(string jmeno, string prijmeni, string rodneCislo, string datumNarozeni) {
+            this.jmeno = jmeno;
+            this.prijmeni = prijmeni;
+            this.rodneCislo = rodneCislo;
+            this.datumNarozeni = datumNarozeni;
+            matrikaList.Add(this);
+        }
+
+        public static void Svatba(Matrika a, Matrika b, string prijmeni) {
+            a.prijmeni = prijmeni;
+            b.prijmeni = prijmeni;
+        }
+        public static void VypisJmena() {
+            foreach (var item in matrikaList) {
+                Console.WriteLine("{0} {1}", item.jmeno, item.prijmeni);
+            }
+        }
+
+        
+    }
+}
---
+++ 1ITB_S2/PVA/02.05.22/PisemnaPrace/PisemnaPrace/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PisemnaPrace {
+    class Program
+    {
+        static Matrika zenska = new Matrika("Irena", "Kosova", "123123/0000", "12.03.2012");
+        static Matrika muz = new Matrika("Ilaloi", "Prasnic", "123123/1111", "12.03.2012");
+        static void Main(string[] args)
+        {
+            while (true) {
+                Dialog();    
+            }
+        }
+        public static void Dialog()
+        {
+            Console.WriteLine("1. Změn příjmení");
+            Console.WriteLine("2. Vypiš uživatele");
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    Console.WriteLine("Jaké má být příjmení?");
+                    string novePrijmeni = Console.ReadLine();
+                    Matrika.Svatba(zenska, muz, novePrijmeni);
+                    break;
+                case 2:
+                    Matrika.VypisJmena();
+                    break;
+                default: 
+                    Console.WriteLine("chyba");
+                    break;
+            }
+        }
+    }
+}
